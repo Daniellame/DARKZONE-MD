d66c2b6f4840f652212528dc3f9cc6cb26c1f2c7
Daniellame <129336307+Daniellame@users.noreply.github.com>
2025-10-03 23:59:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "IK~ntAyXSgA#KaFzEt_YIuw6vHnmC_KvAafkJYCTpnYG68uTi89N8ok",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -13,27 +13,27 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY DARKZONE-MD 🤍*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY ༺𒆜༆Dᴀɴɢᴇʀᴏᴜs𓀿MD༆𒆜༻ 🤍*",
 // set the auto reply massage on status reply  
-ANTI_DELETE: process.env.ANTI_DELETE || "true",
+ANTI_DELETE: process.env.ANTI_DELETE || "false",
 // set true false for anti delete     
 ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
 // change it to 'same' if you want to resend deleted message in same chat     
-WELCOME: process.env.WELCOME || "false",
+WELCOME: process.env.WELCOME || "true",
 // true if want welcome and goodbye msg in groups    
 ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
 // make true to know who dismiss or promoted a member in group
-ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_LINK: process.env.ANTI_LINK || "false",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/4964gx.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://photos.app.goo.gl/63qT1N7UE1peEgvQ8",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "DARKZONE-MD",
+BOT_NAME: process.env.BOT_NAME || "༺𒆜༆Dᴀɴɢᴇʀᴏᴜs𓀿MD༆𒆜༻",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "DARKZONE-MD",
+STICKER_NAME: process.env.STICKER_NAME || "༺𒆜༆Dᴀɴɢᴇʀᴏᴜs𓀿MD༆𒆜༻",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -41,15 +41,15 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923306137477",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "2349015612194",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟",
+OWNER_NAME: process.env.OWNER_NAME || "༺𒆜༆Dᴀɴɢᴇʀᴏᴜs𓀿Hᴀᴄᴋᴇʀ༆𒆜༻",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© CREATER 𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟 *",
+DESCRIPTION: process.env.DESCRIPTION || "*© CREATER ༺𒆜༆Dᴀɴɢᴇʀᴏᴜs𓀿Hᴀᴄᴋᴇʀ༆𒆜༻ *",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/4964gx.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> HEY IM ALIVE NOW  *DARKZONE-MD*⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> HEY IM ALIVE NOW  *༺𒆜༆Dᴀɴɢᴇʀᴏᴜs𓀿MD༆𒆜༻*⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -65,15 +65,15 @@ AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 // maks true for always online 
 PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 // make false if want private mod
-AUTO_TYPING: process.env.AUTO_TYPING || "true",
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923306137477",
+DEV: process.env.DEV || "2349015612194",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
